2289ebf36f2919d410d96798a6720efa7078c6fe
veselin <v.j.vasilev95@gmail.com>
2016-03-15 08:49:00 +0000

    git made me do it

---
+++ fhs/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fhs', '0003_auto_20160312_1257'),
+        ('fhs', '0002_category_description'),
+    ]
+
+    operations = [
+    ]
