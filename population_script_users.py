1e8e69e5d8c18dd86e472764cdeb7c37792c3027
veselin <v.j.vasilev95@gmail.com>
2016-03-21 10:58:04 +0000

    Added scores explanation

--- population_script_users.py
+++ population_script_users.py
@@ -10,43 +10,11 @@ django.setup()
 from fhs.models import UserProfile
 from django.contrib.auth.models import User
 
-def populate_users_non_special():
-    f1 = open("users.txt", "r")
-    line = f1.readline()[: -1]
-    while len(line) != 0:
-        list = string.split(line, " ")
-        family_name = list[0]
-        given_name = list[1]
-        initial = list[2]
-        gender = list[3]
-        email = "{}.{}@fmail.com".format(given_name, family_name)
-        #create a "random" purely numeric password
-        password = ""
-        for i in range(20):
-            password += str(random.randint(0, 9))
-        #create the user object
-        theUser = User()
-        theUser.username = "{}.{}".format(given_name, family_name)
-        theUser.first_name = given_name
-        theUser.last_name = family_name
-        theUser.email = email
-        theUser.password = password
-        #hash the password
-        theUser.set_password(theUser.password)
 
-        #save the user to the db
-        theUser.save()
-
-        #create the userProfile object
-        profile = UserProfile()
-        profile.user = theUser
-        profile.save()
-        line = f1.readline()[: -1]
-
-    #Also add the three users from the email
+# Add the three special users
 
 def populate_users_special():
-    f1 = open("users_special(jil, bob, jen).txt")
+    f1 = open("users.txt")
     line = f1.readline()[: -1]
     while len(line) != 0:
         theUser = User()
@@ -64,7 +32,6 @@ def populate_users_special():
 
 if __name__ == '__main__':
     print "Starting FHS user population script..."
-    populate_users_non_special()
     populate_users_special()
 
 
